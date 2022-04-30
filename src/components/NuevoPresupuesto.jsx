ca61bfc26ff9cfb43e81e10eb2306bfb6758c2d3
JoeBlackNinja <jose.nava@alumnos.udg.mx>
2022-04-29 23:36:44 -0500

    Video 96 00:00. Formulario de presupuesto creado

---
+++ src/components/NuevoPresupuesto.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const NuevoPresupuesto = ({presupuesto, setPresupuesto}) => {
+  return (
+    <div className='contenedor-presupuesto contenedor sombra'>
+      <form className='formulario'>
+        <div className='campo'>
+          <label>Definir Presupuesto</label>
+          <input
+            className='nuevo-presupuesto'
+            type="text"
+            placeholder='Anade tu presupuesto'
+            value={presupuesto}
+            onChange={ e => setPresupuesto(e.target.value)}
+          />
+        </div>
+        <input type="submit" value="Añadir"/>
+      </form>
+    </div>
+  )
+}
+
+export default NuevoPresupuesto
